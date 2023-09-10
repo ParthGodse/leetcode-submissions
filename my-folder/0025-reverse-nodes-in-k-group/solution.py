ff18c76bffab6b7104505efd2f373e34d1e8ac1a
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2023-09-10 22:16:04 +0000

    [LeetCode Sync] Runtime - 19 ms (6.89%), Memory - 15.2 MB (5.26%)

---
+++ my-folder/0025-reverse-nodes-in-k-group/solution.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        count=0
+        temp=head
+        while temp:
+            temp=temp.next
+            count+=1
+        n=count//k #No. of groups to be reversed
+        prev=dummy=ListNode()
+        dummy.next=head
+        while n:
+            curr=prev.next
+            nex=curr.next
+            for i in range(1,k): #If we have to reverse k nodes then k-1 links will be reversed
+                curr.next=nex.next
+                nex.next=prev.next
+                prev.next=nex
+                nex=curr.next
+            prev=curr
+            n-=1
+        return dummy.next
