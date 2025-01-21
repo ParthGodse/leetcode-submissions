a9f125543c27e1c9acbfe59b75cda821261cc6cc
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-01-21 23:49:18 +0000

    [LeetCode Sync] Runtime - 7 ms (89.32%), Memory - 17.7 MB (97.52%)

---
+++ my-folder/0242-valid-anagram/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count1 = {}
+        count2 = {}
+        for char in s:
+            count1[char] = count1.get(char, 0) + 1
+        for char in t:
+            count2[char] = count2.get(char, 0) + 1
+
+        if count1 == count2:
+            return True
+        return False
