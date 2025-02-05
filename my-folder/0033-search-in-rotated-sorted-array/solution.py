2b868138a74c4f8d4c6d3b3035df94f2e3c58337
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-02-05 00:09:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (66.76%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,13 +1,21 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
             else:
-                continue
-        return(-1)
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+
