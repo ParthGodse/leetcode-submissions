4cf4530c4db92bd7dfe8799ef3ae15d887c66d4a
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-02-09 23:34:32 +0000

    [LeetCode Sync] Runtime - 3 ms (87.14%), Memory - 19.2 MB (71.74%)

---
+++ my-folder/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        hmap = {value: idx for idx, value in enumerate(inorder)}
+
+        def dfs(preorder, l, r):
+            if not preorder or l > r:
+                return None
+            
+            root_value = preorder.pop(0)
+            root = TreeNode(root_value)
+            root_index = hmap[root_value]
+
+            root.left = dfs(preorder, l, root_index - 1)
+            root.right = dfs(preorder, root_index + 1, r)
+
+            return root
+        return dfs(preorder, 0, len(inorder) - 1)
