9e0af370fe6d1fc152d56acc9b4dd8e83d39bbae
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-03-02 17:58:59 +0000

    [LeetCode Sync] Runtime - 1 ms (28.11%), Memory - 18.1 MB (19.09%)

---
+++ my-folder/2707-merge-two-2d-arrays-by-summing-values/solution.py
@@ -0,0 +1,38 @@
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        # hashmap = {}
+
+        # for id_, value in nums1:
+        #     hashmap[id_] = hashmap.get(id_, 0) + value
+
+        # for id_, value in nums2:
+        #     hashmap[id_] = hashmap.get(id_, 0) + value
+
+        # return [[key, value] for key, value in sorted(hashmap.items())]
+        i, j  = 0, 0
+        res = []
+
+        while i < len(nums1) and j < len(nums2):
+            id1, val1 = nums1[i]
+            id2, val2 = nums2[j]
+
+            if id1 < id2:
+                res.append([id1, val1])
+                i += 1
+            elif id1 > id2:
+                res.append([id2, val2])
+                j += 1
+            else:
+                res.append([id1, val1 + val2])
+                i += 1
+                j += 1
+
+        while i < len(nums1):
+            res.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            res.append(nums2[j])
+            j += 1
+
+        return res
