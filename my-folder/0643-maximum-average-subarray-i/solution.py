330cfc2f18d926b8ff7cf9a7282b2f929993ff20
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-03-12 21:29:14 +0000

    [LeetCode Sync] Runtime - 89 ms (42.14%), Memory - 27.3 MB (95.70%)

---
+++ my-folder/0643-maximum-average-subarray-i/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if len(nums) == k:
+            return sum(nums)/k
+
+        window = sum(nums[:k])
+        max_res = window / k
+        for i in range(k, len(nums)):
+            window += nums[i] - nums[i - k]
+            res = window/k
+            max_res = max(res, max_res)
+        
+        return max_res
