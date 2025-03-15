3476527fadd3dfc682ca8a5587aa0acfa46f22ff
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-03-15 22:51:51 +0000

    [LeetCode Sync] Runtime - 4 ms (72.84%), Memory - 18.6 MB (77.81%)

--- my-folder/0724-find-pivot-index/solution.py
+++ my-folder/0724-find-pivot-index/solution.py
@@ -1,17 +1,28 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        leftSum = [0] * len(nums)  
-        rightSum = [0] * len(nums)
+        # leftSum = [0] * len(nums)  
+        # rightSum = [0] * len(nums)
 
-        leftSum[0] = nums[0]  
-        rightSum[-1] = nums[-1]
-        for i in range(1, len(nums)):
-            leftSum[i] = leftSum[i - 1] + nums[i] 
+        # leftSum[0] = nums[0]  
+        # rightSum[-1] = nums[-1]
+        
+        # for i in range(1, len(nums)):
+        #     leftSum[i] = leftSum[i - 1] + nums[i] 
 
-        for i in range(len(nums) - 2, -1, -1):
-            rightSum[i] = rightSum[i + 1] + nums[i]
+        # for i in range(len(nums) - 2, -1, -1):
+        #     rightSum[i] = rightSum[i + 1] + nums[i]
+
+        # for i in range(len(nums)):
+        #     if leftSum[i] == rightSum[i]:
+        #         return i
+        # return -1
+        leftSum = 0
+        rightSum = sum(nums)
 
         for i in range(len(nums)):
-            if leftSum[i] == rightSum[i]:
+            rightSum -= nums[i]
+            if leftSum == rightSum:
                 return i
+            leftSum += nums[i]
         return -1
+
