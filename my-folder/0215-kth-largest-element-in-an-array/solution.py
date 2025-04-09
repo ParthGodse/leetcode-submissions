52acb36bcd83fc9a55f6241651140bb6a9b1938f
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-04-09 21:04:21 +0000

    [LeetCode Sync] Runtime - 99 ms (36.45%), Memory - 28.5 MB (81.92%)

--- my-folder/0215-kth-largest-element-in-an-array/solution.py
+++ my-folder/0215-kth-largest-element-in-an-array/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # 
-        heap = nums[:k]
-        heapq.heapify(heap)
+        minheap = []
+        for i in nums:
+            minheap.append(i)
+        heapq.heapify(minheap)  
+        while len(minheap) > k:
+            heapq.heappop(minheap)
+        return minheap[0]
+        # heap = nums[:k]
+        # heapq.heapify(heap)
         
-        for num in nums[k:]:
-            if num > heap[0]:
-                heapq.heappop(heap)
-                heapq.heappush(heap, num)
+        # for num in nums[k:]:
+        #     if num > heap[0]:
+        #         heapq.heappop(heap)
+        #         heapq.heappush(heap, num)
         
-        return heap[0]
+        # return heap[0]
         #return heapq.nlargest(k, nums)[-1]
