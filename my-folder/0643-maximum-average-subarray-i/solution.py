4e5a95c51d5d3f61a76732b5713694cf24c30ae1
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-04-17 22:45:49 +0000

    [LeetCode Sync] Runtime - 79 ms (65.45%), Memory - 27.4 MB (77.67%)

--- my-folder/0643-maximum-average-subarray-i/solution.py
+++ my-folder/0643-maximum-average-subarray-i/solution.py
@@ -3,11 +3,19 @@ class Solution:
         if len(nums) == k:
             return sum(nums)/k
 
+        # window = sum(nums[:k])
+        # max_res = window / k
+        # for i in range(k, len(nums)):
+        #     window += nums[i] - nums[i - k]
+        #     res = window/k
+        #     max_res = max(res, max_res)
+        
+        # return max_res
         window = sum(nums[:k])
-        max_res = window / k
+        maxx = window
+
         for i in range(k, len(nums)):
             window += nums[i] - nums[i - k]
-            res = window/k
-            max_res = max(res, max_res)
-        
-        return max_res
+            maxx = max(maxx, window)
+
+        return maxx / k
