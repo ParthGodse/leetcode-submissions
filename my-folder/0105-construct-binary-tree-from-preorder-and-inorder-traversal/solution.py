95c7d58ecb01456cea3ddde21a3788fa0e547f43
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-19 23:15:02 +0000

    [LeetCode Sync] Runtime - 99 ms (25.29%), Memory - 89.4 MB (20.09%)

--- my-folder/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ my-folder/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -6,18 +6,27 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        hmap = {value: idx for idx, value in enumerate(inorder)}
+        # hmap = {value: idx for idx, value in enumerate(inorder)}
 
-        def dfs(preorder, l, r):
-            if not preorder or l > r:
-                return None
+        # def dfs(preorder, l, r):
+        #     if not preorder or l > r:
+        #         return None
             
-            root_value = preorder.pop(0)
-            root = TreeNode(root_value)
-            root_index = hmap[root_value]
+        #     root_value = preorder.pop(0)
+        #     root = TreeNode(root_value)
 
-            root.left = dfs(preorder, l, root_index - 1)
-            root.right = dfs(preorder, root_index + 1, r)
+        #     root_index = hmap[root_value]
 
-            return root
-        return dfs(preorder, 0, len(inorder) - 1)
+        #     root.left = dfs(preorder, l, root_index - 1)
+        #     root.right = dfs(preorder, root_index + 1, r)
+
+        #     return root
+        # return dfs(preorder, 0, len(inorder) - 1)
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
