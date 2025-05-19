7d21b56aaa8498039e41e6349037f3223bf8004f
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-19 18:17:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (33.80%)

--- my-folder/0075-sort-colors/solution.py
+++ my-folder/0075-sort-colors/solution.py
@@ -8,25 +8,36 @@ class Solution:
         #     for j in range(i+1, n):
         #         if nums[j] < nums[i]:
         #             nums[i], nums[j] = nums[j], nums[i]
-        l, r = 0, n - 1
-        while l <= r:
-            if nums[l] == 0:
-                l += 1
-            elif nums[r] != 0:
-                r -= 1
-            else:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
+        # l, r = 0, n - 1
+        # while l <= r:
+        #     if nums[l] == 0:
+        #         l += 1
+        #     elif nums[r] != 0:
+        #         r -= 1
+        #     else:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r -= 1
 
-        r = n - 1
-        while l <= r:
-            if nums[r] == 2:
-                r -= 1
-            elif nums[l] != 2:
+        # r = n - 1
+        # while l <= r:
+        #     if nums[r] == 2:
+        #         r -= 1
+        #     elif nums[l] != 2:
+        #         l += 1
+        #     else:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r -= 1
+        # return nums
+        l, mid, r = 0, 0, len(nums) - 1
+        while mid <= r:
+            if nums[mid] == 0:
+                nums[l], nums[mid] = nums[mid], nums[l]
+                mid += 1
                 l += 1
+            elif nums[mid] == 1:    
+                mid += 1
             else:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
+                nums[mid], nums[r] = nums[r], nums[mid]
                 r -= 1
-        return nums
