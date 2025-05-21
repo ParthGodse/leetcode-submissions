03128381b30c5f3b6ea6e655452c2a32ab75d232
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-21 13:11:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (88.72%)

---
+++ my-folder/0058-length-of-last-word/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
+        i = n - 1
+        count = 0
+
+        while i >= 0 and s[i] == " ":
+            i -= 1
+        
+        while i >= 0 and s[i] != " ":
+            i -= 1
+            count += 1
+        return count
