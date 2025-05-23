7d648db96723559251838e25c1634722227bfba6
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-23 20:11:44 +0000

    [LeetCode Sync] Runtime - 7 ms (41.85%), Memory - 18.8 MB (79.80%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -4,13 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left, right = 0, 1
-        i = 0 
         for right in range(len(nums)):
-
-            # if nums[left] != 0:
-            #     left = right
-            # if nums[right] == 0:
-            #     continue
             if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
