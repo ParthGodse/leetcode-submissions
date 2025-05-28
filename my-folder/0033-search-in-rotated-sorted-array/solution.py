0bb4d9d1adc779ca8fc501a58f145ff3705f273d
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-28 23:16:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.3 MB (22.52%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,21 +1,37 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
+        # low, high = 0, len(nums) - 1
+        # while low <= high:
+        #     mid = (low + high) // 2
+        #     if nums[mid] == target:
+        #         return mid
+
+        #     if nums[low] <= nums[mid]:
+        #         if nums[low] <= target < nums[mid]:
+        #             high = mid - 1
+        #         else:
+        #             low = mid + 1
+        #     else:
+        #         if nums[mid] < target <= nums[high]:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        # return -1
+
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r)//2
             if nums[mid] == target:
                 return mid
 
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target < nums[mid]:
-                    high = mid - 1
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
                 else:
-                    low = mid + 1
+                    l = mid + 1
             else:
-                if nums[mid] < target <= nums[high]:
-                    low = mid + 1
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
                 else:
-                    high = mid - 1
+                    r = mid - 1
         return -1
-
-
