2bb6566953093de16fc5674b37e3b32ff40746bb
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-05-28 22:47:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.7 MB (73.61%)

--- my-folder/0792-binary-search/solution.py
+++ my-folder/0792-binary-search/solution.py
@@ -14,17 +14,28 @@ class Solution:
         #     else:
         #         h = m - 1
         # return -1
-        def binary(l, h):
-            if l>h:
-                return -1
-            while l<=h:
-                m = (l + h) //2
-                if nums[m] == target:
-                    return m
-                elif nums[m] < target:
-                    return binary(m+1,h)
-                else:
-                    return binary(l,m-1)
+        # def binary(l, h):
+        #     if l>h:
+        #         return -1
+        #     while l<=h:
+        #         m = (l + h) //2
+        #         if nums[m] == target:
+        #             return m
+        #         elif nums[m] < target:
+        #             return binary(m+1,h)
+        #         else:
+        #             return binary(l,m-1)
 
-        return binary(0,len(nums)-1)
-
+        # return binary(0,len(nums)-1)
+        l, h = 0, len(nums) - 1
+        if l > h:
+            return -1
+        while l <= h:
+            m = (l + h) //2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                h = m - 1
+        return -1
