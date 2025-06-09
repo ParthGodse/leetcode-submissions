18008e6839fb04519b729198cbcd45859fb23fb1
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-06-09 13:46:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (91.66%)

---
+++ my-folder/0140-word-break-ii/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        words = set(wordDict)
+        res = {}
+
+        def dfs(start):
+            if start in res:
+                return res[start]
+
+            if start == len(s):
+                return [""]
+
+            sentences = []
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in words:
+                    rest_sentences = dfs(end)
+                    for rest in rest_sentences:
+                        sentence = word + (" " + rest if rest else "")
+                        sentences.append(sentence)
+
+            res[start] = sentences
+            return sentences
+        return dfs(0)
