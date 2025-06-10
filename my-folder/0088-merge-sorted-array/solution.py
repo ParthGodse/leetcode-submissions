f5147e4f6c773026466cc5c65863437dfadee330
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-06-10 23:55:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (93.55%)

--- my-folder/0088-merge-sorted-array/solution.py
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-        
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+        p1, p2 = m-1, n-1
+        write = m+n-1
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[write] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
+                nums1[write] = nums2[p2]
+                p2 -= 1
+            write -= 1
