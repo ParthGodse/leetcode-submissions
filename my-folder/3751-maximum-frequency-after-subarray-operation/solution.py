79dcfc0d3f1de2525cbc5dafe979b61713fa82ef
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-06-10 13:43:56 +0000

    [LeetCode Sync] Runtime - 5659 ms (9.07%), Memory - 21.4 MB (75.26%)

--- my-folder/3751-maximum-frequency-after-subarray-operation/solution.py
+++ my-folder/3751-maximum-frequency-after-subarray-operation/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        mxx = 0
-        kc = nums.count(k)
-        for i in range(1,51):
-            if i == k: continue
-            mx = 0
-            c = 0
+        maximumfreq = 0
+        currk = nums.count(k)
+        for i in range(51):
+            if i == k:
+                continue
+            currmax = 0
+            curr = 0
             for num in nums:
-                if num == i: c += 1
-                elif num == k: c -= 1
-                mx = max(mx, c)
-                c = max(0, c)
-            mxx = max(mxx, mx)
-        return kc + mxx
+                if i == num:
+                    curr += 1
+                elif num == k:
+                    curr -= 1
+                currmax = max(currmax, curr)
+                curr = max(0, curr)
+            maximumfreq = max(maximumfreq, currmax)
+        return maximumfreq + currk 
