de0e115d8cdfac644f386eb5063984f749883cf1
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-06-10 15:03:13 +0000

    [LeetCode Sync] Runtime - 161 ms (31.30%), Memory - 78.7 MB (8.43%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,17 +1,16 @@
 class Node:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
+        self.key, self.val = key, val
         self.prev = self.next = None
 
 class LRUCache:
-
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
         self.cap = capacity
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.left.next = self.right
+        self.right.prev = self.left
 
     def remove(self, node):
         prev, nxt = node.prev, node.next
@@ -23,15 +22,15 @@ class LRUCache:
         prev.next = node
         nxt.prev = node
         node.next, node.prev = nxt, prev
-    
-    def get(self, key: int) -> int:
+
+    def get(self, key):
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-
-    def put(self, key: int, value: int) -> None:
+    
+    def put(self, key, value):
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
@@ -41,7 +40,6 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
