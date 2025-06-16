9dd41f87759b641e47acdc9dee3290e7e06f1601
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-06-16 18:20:07 +0000

    [LeetCode Sync] Runtime - 5 ms (31.34%), Memory - 17.8 MB (87.27%)

--- my-folder/0139-word-break/solution.py
+++ my-folder/0139-word-break/solution.py
@@ -1,25 +1,12 @@
-# class Solution:
-#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # n = len(s)
-        # dp = [False] * (n + 1)
-        # dp[0] = True
-        # max_len = max(map(len, wordDict))
-
-        # for i in range(1, n + 1):
-        #     for j in range(i - 1, max(i - max_len - 1, -1), -1):
-        #         if dp[j] and s[j:i] in wordDict:
-        #             dp[i] = True
-        #             break
-        # return dp[n]
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
         words = set(wordDict)
-        queue = deque([0])
+        q = deque([0])
         seen = set()
 
-        while queue:
-            start = queue.popleft()
+        while q:
+            start = q.popleft()
             if start == len(s):
                 return True
 
@@ -28,7 +15,6 @@ class Solution:
                     continue
 
                 if s[start:end] in words:
-                    queue.append(end)
+                    q.append(end)
                     seen.add(end)
-
         return False
