0e2948231792fe5aecfc4fa7bf04d7da4f9a5ece
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-10-23 17:42:02 -0400

    Time: 7 ms (24.09%), Space: 18.1 MB (24.92%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,21 +1,32 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+        # dp = [False] * (len(s) + 1)
+        # dp[len(s)] = True
 
-        for i in range(len(s) - 1, -1, -1):
-            for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i:i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]
-                if dp[i]:
-                    break
+        # for i in range(len(s) - 1, -1, -1):
+        #     for w in wordDict:
+        #         if (i + len(w)) <= len(s) and s[i:i + len(w)] == w:
+        #             dp[i] = dp[i + len(w)]
+        #         if dp[i]:
+        #             break
 
-        return dp[0]
+        # return dp[0]
         
-        # words = set(wordDict)
-        # q = deque([words[0]])
-        # i = 0
-        # while q:
+        words = set(wordDict)
+        q = deque([0])
+        visit = set()
+        while q:
+            start = q.popleft()
+            if start == len(s):
+                return True
 
-        #     if   
+            for end in range(start + 1, len(s) + 1):
+                if end in visit:
+                    continue
+
+                if s[start:end] in words:
+                    q.append(end)
+                    visit.add(end)
+
+        return False   
