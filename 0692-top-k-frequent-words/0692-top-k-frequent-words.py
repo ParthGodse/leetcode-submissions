9d0efbbff024efdd5d2095705c603be33391df2a
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-11-03 17:09:05 -0500

    Time: 0 ms (100%), Space: 18.1 MB (20.82%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+
+        heap = [(-f, w) for w, f in freq.items()]
+        heapq.heapify(heap)
+
+        return [heapq.heappop(heap)[1] for _ in range(k)]
