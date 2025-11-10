dad385b5a87929ea9ba3946c31173433602e9c25
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-11-10 13:00:44 -0500

    Time: 39 ms (59.23%), Space: 17.7 MB (82.94%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,21 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x > 2**31 - 1:
-            return 0
-        stack = []
-        res = []
-        s = str(x)
-        for ch in s:
-            if ch.isdigit():
-                stack.append(ch)
-            else:
-                res.append(ch)
-        print(stack)
-        while stack:    
-            res.append(stack.pop())
-        print(res)
-        new =  "".join(res)
-        if new == "":
-            return 0
-        new1= int(new) 
-        return new1 if abs(new1) < 2**31 - 1 else 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = int(str(x)[::-1]) * sign
+
+        return rev if -2**31 <= rev <= 2**31 - 1 else 0
