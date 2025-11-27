cb29aeeb786890ff294293022cf62b3e804768d4
Parth Godse <98154485+ParthGodse@users.noreply.github.com>
2025-11-27 13:20:22 -0500

    Time: 50 ms (27.6%), Space: 18.1 MB (57.18%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -3,13 +3,14 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
-        dp[len(s1)][len(s2)] = [True]  
+        n1, n2 = len(s1), len(s2)
+        dp = [[False] * (n2 + 1) for i in range(n1 + 1)]
+        dp[n1][n2] = True 
 
-        for i in range(len(s1), -1, -1):
-            for j in range(len(s2), -1, -1):
-                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+        for i in range(n1, -1, -1):
+            for j in range(n2, -1, -1):
+                if i < n1 and s1[i] == s3[i + j] and dp[i + 1][j]:
                     dp[i][j] = True
-                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                if j < n2 and s2[j] == s3[i + j] and dp[i][j + 1]:
                     dp[i][j] = True
         return dp[0][0]
